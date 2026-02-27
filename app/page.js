a7475eae215f6ffb279283569c24ab7b83de13c0
wizzbuzz <joop_hooijmans@outlook.com>
2026-02-27 13:47:12 +0100

    Basic Layout

--- app/page.js
+++ app/page.js
@@ -2,9 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div id="header" className="b">
-
+    <div className="min-h-screen bg-zinc-50 font-sans text-white">
+      <div id="header" className="flex justify-center p-2 bg-gray-900 w-full h-10%">
+        <img src="Logo.png" className="w-[20%]"/>
+      </div>
+      <div id="header" className="bg-gray-900 w-full flex justify-center">
+        <div id="game" className="w-200 h-100 bg-white p-10">
+            <p>
+              Game
+            </p>
+          </div>
+          
       </div>
     </div>
   );
